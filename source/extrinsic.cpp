b530a64b3e0345e32785f1741b0e1632baeff5c0
wildjerry <71886533+wildjerry@users.noreply.github.com>
2021-12-30 19:50:00 -0600

    Implemented Circle Detection

--- source/extrinsic.cpp
+++ source/extrinsic.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using namespace cv;
 
+const float PI = 3.14159;
 /* COLORS DO NOT GET PROPERLY SET!
 Mat CreateTestImage() {
     Mat ret = Mat3f(500,500,0.0f);
@@ -15,11 +16,15 @@ Mat CreateTestImage() {
 }
 */
 
+int numCircles;
+float radius,maxApparentRadius;
+Point2f center,closeCenter;
+
 int main()
 {
     char str[50];
 
-    Mat img, draw;
+    Mat img, imgHSV,draw;
 
     // -- USE CAMERA --
 
@@ -48,8 +53,8 @@ int main()
 
         //img=CreateTestImage();
         //img = imread("./images/1.jpg", 1);
-        img = imread("./images/2.jpg", 1);
-        //img = imread("./images/3.jpg", 1);
+        //img = imread("./images/2.jpg", 1);
+        img = imread("./images/3.jpg", 1);
 
         // -- END USE SAVED IMAGE --
         // safety check to see if img has stuff
@@ -66,60 +71,66 @@ int main()
         // threshold pixels with a value of 250 or greater - these values will need to be tuned. Start broad and close it down
         //inRange(img, 150, 255, img);
         // example of thresholdign in color: BGR
-        inRange(img, Scalar(0, 100, 50), Scalar(200, 255, 255), img);
+        //inRange(img, Scalar(0, 100, 50), Scalar(200, 255, 255), img);
+
+        //HSV Thresholding
+        cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+        imshow("HSV as RGB", imgHSV);
+
+        inRange(imgHSV, Scalar(14,70,88), Scalar(50,254,255), img);
+
         imshow("Threshold", img);
 
-        // erode and dilate once
-        erode(img, img, kern, Point(-1,-1), 1);
-        dilate(img, img, kern, Point(-1,-1), 1);
+        // erode and dilate twice
+        erode(img, img, kern, Point(-1,-1), 2);
+        dilate(img, img, kern, Point(-1,-1), 2);
+
+        //dilate and erode twice
+        dilate(img, img, kern, Point(-1,-1), 2);
+        erode(img, img, kern, Point(-1,-1), 2);
 
         vector<vector<Point> > contours;
         vector<Vec4i> hierarchy;
-        //findContours(img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+        findContours(img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-        vector<Vec3f> circles;
-        cout << circles.size() << " Tennis Balls were found in the image.\n";
 
-        int largestApparentRadius = 0;
-        Point closest;
-
-        HoughCircles(img, circles, HOUGH_GRADIENT, 1,
-                     img.rows/16,  // change this value to detect circles with different distances to each other
-                     100, 30, 1, img.rows // change the last two parameters
-                // (min_radius & max_radius) to detect larger circles
-        );
-
-        for( size_t i = 0; i < circles.size(); i++ )
-        {
-            Vec3i c = circles[i];
-            Point center = Point(c[0], c[1]);
-            // circle center
-            circle( draw, center, 1, Scalar(0,100,100), 3, LINE_AA);
-            // circle outline
-            int radius = c[2];
-            if (radius>largestApparentRadius){
-                largestApparentRadius = radius;
-                closest = center;
-            }
-
-            circle( draw, center, radius, Scalar(255,0,255), 3, LINE_AA);
-        }
+        maxApparentRadius = 0;
+        numCircles = 0;
 
         for (size_t i = 0; i < contours.size(); i++) {
+            drawContours(draw, contours, (int)i, Scalar(0, 255, 0), 2, LINE_8, hierarchy, 0);
 
+            minEnclosingCircle(contours[i], center,radius);
 
-            //drawContours(draw, contours, (int)i, Scalar(0, 255, 0), 2, LINE_8, hierarchy, 0);
+            float circleArea = PI * radius * radius;
+            float actualArea = contourArea(contours[i]); //contour area
 
+            double circleness = actualArea/circleArea; //aproaches 1 as shape becomes more circular
+            if (circleness < 0.9){
+                continue;
+            }
+
+            numCircles++;
+            if (radius>maxApparentRadius){
+                closeCenter=center;
+                maxApparentRadius = radius;
+            }
         }
 
         // Display image
         imshow("Final", draw);
-        
+
         // wait for key stroke
         char c = waitKey(10);
 
         if (c == 27) {
             break;
         }
+    }
+
+    cout << numCircles << " " << "Tennis balls detected." << " ";
+    if(numCircles>0){
+        cout << "The closest tennis ball was at " << closeCenter << ".\n";
     }
 }
